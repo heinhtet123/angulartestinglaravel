31e6075adfdff72a8919c524a57a97c37b369d37
heinhtet <heinhtetstarfall@gmail.com>
2016-09-11 17:21:16 +0630

    added sorting ajax

--- public/app/controllers/ItemController.js
+++ public/app/controllers/ItemController.js
@@ -8,17 +8,32 @@ app.controller('ItemController',function(dataFactory,$log,$scope,$window,$http){
 //	global_var=1;
   $scope.sortField="id";
   $scope.reverse=true;
+  $scope.params="?sortby='desc'&";
 
-  $scope.sort=function (name){
-    $scope.sortField=name;
+  $scope.sort=function (sortname){
+    $scope.sortField=sortname;
     $scope.reverse=!$scope.reverse;
+    $scope.loading=true;
+    // if true reverse=true if reverse=false
 
-    console.log($scope.reverse);
+      if($scope.reverse==true)
+      { 
+       $scope.params="?sortby='desc'&";
+
+      }else
+      {
+        $scope.params="?";
+      }
+
+     dataFactory.httpRequest("/items"+$scope.params+"page="+$scope.bigCurrentPage+" ").then(function(data){
+        $scope.loading=false;
+        $scope.data=data;
+      });   
+
   };
 
 
   $scope.loading=true;
-
   //items per page
   $scope.itemsperpage=5;
 
@@ -28,7 +43,8 @@ app.controller('ItemController',function(dataFactory,$log,$scope,$window,$http){
     $scope.loading=true;
 
     // ajax call to page
-    dataFactory.httpRequest("/items?sortby='desc'&page="+pageNo+" ").then(function(data){
+
+    dataFactory.httpRequest("/items"+$scope.params+"page="+pageNo+" ").then(function(data){
       $scope.loading=false;
       $scope.data=data;
       
